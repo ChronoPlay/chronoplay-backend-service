ad489ecef292a0af5ce596c82dc2020c17a8b759
Shivam Chaudhary <163902104+shivamcy@users.noreply.github.com>
2025-08-15 01:26:29 +0530

    user login via email or username

--- mapper/userMapper.go
+++ mapper/userMapper.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/ChronoPlay/chronoplay-backend-service/helpers"
 	model "github.com/ChronoPlay/chronoplay-backend-service/model"
+	utils "github.com/ChronoPlay/chronoplay-backend-service/utils"
 )
 
 func DecodeRegisterUserRequest(r *gin.Context) (user model.User, err *helpers.CustomError) {
@@ -37,19 +38,27 @@ func DecodeVerifyUserRequest(r *gin.Context) (req model.VerifyUserRequest, err *
 	return req, nil
 }
 
-func DecodeLoginUser(r *gin.Context) (req model.LoginUserRequest, err *helpers.CustomError) {
+func DecodeLoginUserRequest(r *gin.Context) (req model.LoginUserRequest, err *helpers.CustomError) {
+	
 	if err := r.ShouldBindJSON(&req); err != nil {
 		return model.LoginUserRequest{}, helpers.BadRequest("Invalid request: " + err.Error())
 	}
+	if req.Identifier!=""{
+		if utils.IsValidEmail(req.Identifier){
+			req.Email=req.Identifier
+		} else{
+			req.UserName=req.Identifier
+		}
+	}
 
-	if req.Email == "" && req.PhoneNumber == "" {
-		return model.LoginUserRequest{}, helpers.BadRequest("Missing email or phone_number in request body")
+	if req.Email == "" && req.UserName == "" {
+		return model.LoginUserRequest{}, helpers.BadRequest("Missing email or user_name in request body")
 	}
 
 	if req.Password == "" {
 		return model.LoginUserRequest{}, helpers.BadRequest("Missing password in request body")
 	}
 
-	log.Println("Parsed login request with email:", req.Email, "and phone number:", req.PhoneNumber)
+	log.Println("Parsed login request with email:", req.Email, "and user_name:", req.UserName)
 	return req, nil
 }
