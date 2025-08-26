1de95a736453c9231db0fd967164ff818c5ebe8b
sparshkumar007 <sparshrajput92@gmail.com>
2025-08-27 00:26:00 +0530

    logs test fixed

--- middlewares/userMiddleware.go
+++ middlewares/userMiddleware.go
@@ -27,35 +27,39 @@ func NewUserMiddleware(next service.UserService) UserMiddleware {
 
 func (mw userMiddleware) GetUser(ctx context.Context, req dto.GetUserRequest) (resp dto.GetUserResponse, err *helpers.CustomError) {
 	defer func(begin time.Time) {
-		log.Printf("ctx:", ctx, " method:", "GetUser", " req:", req, " took:", time.Since(begin), " err:", err, " resp:", resp)
+		log.Printf("ctx:%v method:%v req:%v took:%v err:%v resp:%v",
+			ctx, "GetUser", req, time.Since(begin), err, resp)
 	}(time.Now())
 	return mw.next.GetUser(ctx, req)
 }
 
 func (mw userMiddleware) RegisterUser(ctx context.Context, req model.User) (err *helpers.CustomError) {
 	defer func(begin time.Time) {
-		log.Printf("ctx:", ctx, " method:", "RegisterUser", " req:", req, " took:", time.Since(begin), " err:", err)
+		log.Printf("ctx:%v method:%v req:%v took:%v err:%v",
+			ctx, "RegisterUser", req, time.Since(begin), err)
 	}(time.Now())
 	return mw.next.RegisterUser(ctx, req)
 }
 
 func (mw userMiddleware) VerifyUser(ctx context.Context, req dto.VerifyUserRequest) (err *helpers.CustomError) {
 	defer func(begin time.Time) {
-		log.Printf("ctx:", ctx, " method:", "RegisterUser", " req:", req, " took:", time.Since(begin), " err:", err)
+		log.Printf("ctx:%v method:%v req:%v took:%v err:%v",
+			ctx, "VerifyUser", req, time.Since(begin), err)
 	}(time.Now())
 	return mw.next.VerifyUser(ctx, req)
 }
 
 func (mw userMiddleware) LoginUser(ctx context.Context, req dto.LoginUserRequest) (resp dto.LoginUserResponse, err *helpers.CustomError) {
 	defer func(begin time.Time) {
-		log.Printf("ctx:", ctx, " method:", "LoginUser", " req:", req, " took:", time.Since(begin), " err:", err, " resp:", resp)
+		log.Printf("ctx:%v method:%v req:%v took:%v err:%v resp:%v",
+			ctx, "LoginUser", req, time.Since(begin), err, resp)
 	}(time.Now())
 	return mw.next.LoginUser(ctx, req)
 }
 func (mw userMiddleware) AddFriend(ctx context.Context, req *dto.AddFriendRequest) (err *helpers.CustomError) {
 	defer func(begin time.Time) {
-		log.Printf("ctx: %v method: AddFriend userID: %d fid: %d took: %s err: %v",
-			ctx, req.UserID, req.FriendID, time.Since(begin), err)
+		log.Printf("ctx:%v method:%v userID:%d friendID:%d took:%v err:%v",
+			ctx, "AddFriend", req.UserID, req.FriendID, time.Since(begin), err)
 	}(time.Now())
 
 	return mw.next.AddFriend(ctx, req)
@@ -63,8 +67,8 @@ func (mw userMiddleware) AddFriend(ctx context.Context, req *dto.AddFriendReques
 
 func (mw userMiddleware) GetFriends(ctx context.Context, req *dto.GetFriendsRequest) (resp []dto.Friend, err *helpers.CustomError) {
 	defer func(begin time.Time) {
-		log.Printf("ctx: %v method: GetFriends userID: %d took: %s err: %v",
-			ctx, req.UserID, time.Since(begin), err)
+		log.Printf("ctx:%v method:%v userID:%d took:%v err:%v",
+			ctx, "GetFriends", req.UserID, time.Since(begin), err)
 	}(time.Now())
 
 	return mw.next.GetFriends(ctx, req)
@@ -89,14 +93,16 @@ func (mw userMiddleware) GetAllActiveUsers() (users []model.User, err *helpers.C
 
 func (mw userMiddleware) UpdateUser(ctx context.Context, user model.User) (err *helpers.CustomError) {
 	defer func(begin time.Time) {
-		log.Printf("ctx:", ctx, " method:", "UpdateUser", " req:", user, " took:", time.Since(begin), " err:", err)
+		log.Printf("ctx:%v method:%v req:%v took:%v err:%v",
+			ctx, "UpdateUser", user, time.Since(begin), err)
 	}(time.Now())
 	return mw.next.UpdateUser(ctx, user)
 }
 
 func (mw userMiddleware) ActivateAllUsers(ctx context.Context) (err *helpers.CustomError) {
 	defer func(begin time.Time) {
-		log.Printf("ctx:", ctx, " method:", "ActivateAllUsers", " took:", time.Since(begin), " err:", err)
+		log.Printf("ctx:%v method:%v took:%v err:%v",
+			ctx, "ActivateAllUsers", time.Since(begin), err)
 	}(time.Now())
 	return mw.next.ActivateAllUsers(ctx)
 }
